a52dc7d9981aae71ee2f531c1e44f10162c3746b
AlanWilliam1986 <alan.william1986@gmail.com>
2025-02-02 19:38:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,10 +7,15 @@ import os
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+print(f"TELEGRAM_BOT_TOKEN: {TELEGRAM_BOT_TOKEN}")
+
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+print(f"OPENAI_API_KEY: {OPENAI_API_KEY}")
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 openai.api_key = OPENAI_API_KEY
+
+
 
 # Função para responder mensagens
 @bot.message_handler(func=lambda message: True)
